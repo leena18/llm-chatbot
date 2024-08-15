20cd84075125e4deb17e5a266dbeeb44650b7e1c
leena ghatiya <leena@valuebound.com>
2024-08-16 02:37:28 +0530

    adding all the content

---
+++ main.py
@@ -0,0 +1,61 @@
+import os
+
+import streamlit as st
+from dotenv import load_dotenv
+import google.generativeai as gen_ai
+
+
+# to load environment variables
+load_dotenv()
+
+# Configure Streamlit page settings
+st.set_page_config(
+    page_title="Chat with AI",
+    page_icon=":pizza:",  # Favicon emoji
+    layout="wide",  # Page layout option
+)
+
+GOOGLE_API_KEY = "AIzaSyBPlnOipZ4X8KzmAdrdLGOM7gmiyi5dizM"
+print("GOOGLE_API_KEY: ", GOOGLE_API_KEY)
+
+# Set up Google Gemini-Pro AI model
+gen_ai.configure(api_key=GOOGLE_API_KEY)
+model = gen_ai.GenerativeModel('gemini-pro')
+
+
+# Function to translate roles between Gemini-Pro and Streamlit terminology
+def translate_role_for_streamlit(user_role):
+    if user_role == "model":
+        return "assistant"
+    else:
+        return user_role
+
+
+# Initialize chat session in Streamlit if not already present
+if "chat_session" not in st.session_state:
+    st.session_state.chat_session = model.start_chat(history=[]) #to main tain the history in this project
+
+
+# Display the chatbot's title on the page
+st.title("🤖 Robo ChatBot")
+
+# Display the chat history
+for message in st.session_state.chat_session.history:
+    with st.chat_message(translate_role_for_streamlit(message.role)):
+        st.markdown(message.parts[0].text)
+
+# Input field for user's message
+user_prompt = st.chat_input("Ask a question!!...")
+if user_prompt:
+    # Add user's message to chat and display it
+    st.chat_message("user").markdown(user_prompt)
+
+    # Send user's message to Gemini-Pro and get the response
+    gemini_response = st.session_state.chat_session.send_message(user_prompt)
+
+    # Display Gemini-Pro's response
+    with st.chat_message("assistant"):
+        st.markdown(gemini_response.text)
+        
+    #show balloon when assistant responds
+    st.balloons()
